10857780594d0ba75ccbf32a2ae7be01cd724621
jeroendh1 <78956933+jeroendh1@users.noreply.github.com>
2021-03-31 13:30:32 +0200

    Add files via upload

--- bungalowpark/__init__.py
+++ bungalowpark/__init__.py
@@ -2,6 +2,12 @@ import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+# from bungalowpark.models import BungalowType, Bungalow, User, Boeking 
+from flask_login import LoginManager, UserMixin, login_required
+from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+
+
 
 app = Flask(__name__)
 
@@ -14,7 +20,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
-@app.route('/')
 
+
+# login = LoginManager()
+# login.init_app(app)
+# login.login_view = 'login'
+
+@app.route('/')
 def index():
     return render_template('home.html')
+
+# @app.route('/list')
+# def list_cur():
+#     # Maak een lijst van alle aanwezige cursisten in de database.
+#     bungalows = BungalowType.query.all()
+#     return render_template('test.html', bungalows=bungalows)
+
+# @app.route('/loginpage', methods=("GET", "POST"))
+# def loginpage():
+#     return render_template('login.html')
+
+# @app.route('/register')
+# def register():
+#     return render_template('register.html')
